46b752b926b41b014748b4ea444bc7170614534c
Aditia Pratama <adotcyber@gmail.com>
2016-09-14 09:51:09 +0700

    add upload photo and resume pdf

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,2 +1,12 @@
 class Applicant < ActiveRecord::Base
+mount_uploader :photo, PhotoUploader
+mount_uploader :resume, ResumeUploader
+
+validates_processing_of :photo
+validate :image_size_validation
+ 
+private
+  def image_size_validation
+    errors[:photo] << "should be less than 500KB" if photo.size > 0.5.megabytes
+  end
 end
