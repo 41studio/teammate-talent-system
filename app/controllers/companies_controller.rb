7383c316529866ccfb7359dbbb4a70093a3e9c3b
Aditia Pratama <adotcyber@gmail.com>
2016-09-15 17:01:43 +0700

    add validation for user and join user company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -47,7 +47,7 @@ class CompaniesController < ApplicationController
   def update
     respond_to do |format|
       if @company.update(company_params)
-        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+        format.html { redirect_to dashboards_path, notice: 'Company was successfully updated.' }
         format.json { render :show, status: :ok, location: @company }
       else
         format.html { render :edit }
@@ -61,7 +61,7 @@ class CompaniesController < ApplicationController
   def destroy
     @company.destroy
     respond_to do |format|
-      format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
+      format.html { redirect_to dashboards_path, notice: 'Company was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
 	belongs_to :job
+	belongs_to :company
 		
   acts_as_token_authenticatable
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable, :recoverable, :rememberable, :trackable, :confirmable, :confirmable
+
+  validates :first_name, :last_name, presence: true
 end
