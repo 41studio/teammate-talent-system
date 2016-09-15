4eade895907f7f6b808a6d31fb7a921ef855e93a
Aditia Pratama <adotcyber@gmail.com>
2016-09-15 09:04:59 +0700

    add user with confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -100,6 +100,13 @@ ActiveRecord::Schema.define(version: 20160915013254) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email",      limit: 255
+    t.integer  "failed_attempts",        limit: 4,   default: 0,  null: false
+    t.string   "unlock_token",           limit: 255
+    t.datetime "locked_at"
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
   end
