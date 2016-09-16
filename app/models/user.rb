227ef50de368e482ca5520906f0e45302babc144
Aditia Pratama <adotcyber@gmail.com>
2016-09-16 10:34:33 +0700

    remove letter_opener gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable, :recoverable, :rememberable, :trackable, :confirmable, :confirmable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, 
+         :confirmable, :confirmable
 
+         
   validates :first_name, :last_name, presence: true
 end
