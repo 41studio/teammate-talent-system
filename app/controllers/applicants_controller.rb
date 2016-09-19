e4a5d360006017b0afe19f7f0a524d30f687466d
kikipratiwi <kitiwi.sch@gmail.com>
2016-09-19 16:34:29 +0700

    authentication on API

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,82 +1,10 @@
-class ApplicantsController < ApplicationController
+Merge branch 'master' of github.com:41studio/teammate-talent-system
+^\
 
-  skip_before_filter :authenticate_user!, only: [:create]
-  before_action :set_applicant, only: [:show, :edit, :update, :destroy]
-  before_action :set_job, only: [:new, :edit]
-  
-  # GET /applicants
-  # GET /applicants.json
-  def index
-    @applicants = Applicant.all
-  end
+# Please enter a commit message to explain why this merge is necessary,
+# especially if it merges an updated upstream into a topic branch.
+#
+# Lines starting with '#' will be ignored, and an empty message aborts
+# the commit.
+^\
 
-  # GET /applicants/1
-  # GET /applicants/1.json
-  def show
-  end
-
-  # GET /applicants/new
-  def new
-    @applicant = Applicant.new
-  end
-
-  # GET /applicants/1/edit
-  def edit
-  end
-
-  # POST /applicants
-  # POST /applicants.json
-  def create
-    @job = Job.find(params[:job_id])
-    @applicant = @job.applicants.new(applicant_params)
-    @applicant.status = "Candidate"
-    respond_to do |format|
-      if @applicant.save
-        format.html { redirect_to job_path(@job), notice: 'Applicant was successfully created.' }
-        format.json { render :show, status: :created, location: @applicant }
-      else
-        format.html { render :new }
-        format.json { render json: @applicant.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /applicants/1
-  # PATCH/PUT /applicants/1.json
-  def update
-    respond_to do |format|
-      if @applicant.update(applicant_params)
-        format.html { redirect_to @applicant, notice: 'Applicant was successfully updated.' }
-        format.json { render :show, status: :ok, location: @applicant }
-      else
-        format.html { render :edit }
-        format.json { render json: @applicant.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /applicants/1
-  # DELETE /applicants/1.json
-  def destroy
-    @applicant.destroy
-    respond_to do |format|
-      format.html { redirect_to applicants_url, notice: 'Applicant was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_applicant
-      @applicant = Applicant.find(params[:id])
-    end
-
-    def set_job
-      @job = Job.find(params[:job_id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def applicant_params
-      params.require(:applicant).permit(:name, :gender, :date_birth, :email, :headline, :phone, :address, :photo, :name_school, :state_study, :degree, :company_name, :industry, :title, :summary, :resume)
-    end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,30 @@
 class User < ActiveRecord::Base
 	belongs_to :company
 	
+	validates :first_name, :last_name, presence: true
+
   acts_as_token_authenticatable
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, 
-         :confirmable, :confirmable         
-  validates :first_name, :last_name, presence: true
+         :confirmable         
+
+  
+  before_save :ensure_authentication_token
+
+  def ensure_authentication_token
+    self.authentication_token ||= generate_authentication_token
+  end
+
+  private
+
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
+
 end
