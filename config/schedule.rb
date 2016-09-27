fccb7b785eb6a986398c14aff709df18ac59d1fd
kikipratiwi <kitiwi.sch@gmail.com>
2016-09-27 17:25:44 +0700

    Heroku Scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,5 @@
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 # runner "MyModel.task_to_run_at_four_thirty_in_the_morning"
 every :day, :at => '11:40 am' do
-runner 'SchedulerApplicantWorker.notify_applicants'
+	runner 'SchedulerApplicantWorker.notify_applicants'
+do
