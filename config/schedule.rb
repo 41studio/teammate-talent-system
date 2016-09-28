5139f72e05849d81a609f6f6224be811ba627107
kikipratiwi <kitiwi.sch@gmail.com>
2016-09-28 09:07:04 +0700

    Update Schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,14 @@
+env :PATH, ENV['PATH']
+# env :GEM_PATH, ENV['GEM_PATH']
+# set :environment, 'development'
+
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
-# runner "MyModel.task_to_run_at_four_thirty_in_the_morning"
-every :day, :at => '11:40 am' do
+every :day, :at => '09:00 am' do
 	runner 'SchedulerApplicantWorker.notify_applicants'
 end
+
+every 1.minutes do
+	rake "notify_applicants"
+end
+
+# /bin/bash -c 'cd /home/kiki/Rails\ Exercises/teammate-talent-system && RAILS_ENV=development bundle exec rake notify_applicants'
