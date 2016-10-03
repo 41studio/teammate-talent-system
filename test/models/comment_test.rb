408217867750481669ff09a348711d3b3d80fa6a
Ilham <ilhamfi_2701@yahoo.com>
2016-10-03 13:23:07 +0700

    annotate on test folder

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id           :integer          not null, primary key
+#  body         :text(65535)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  applicant_id :integer
+#  user_id      :integer
+#
+
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
