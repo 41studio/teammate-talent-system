826fae49d91277415655aa03860afb8acb4ef2ef
Ilham <ilhamfi_2701@yahoo.com>
2016-10-05 08:44:56 +0700

    fix redirecting after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,14 @@ class ApplicationController < ActionController::Base
   def previous_url
     session[:my_previous_url] = URI(request.referer || '').path
   end
+
+  def after_sign_in_path_for(resource)
+  	sign_in_url = new_user_session_url
+    if request.referer == sign_in_url
+      dashboards_path
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
+
 end
--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,6 +1,9 @@
 class LandingPageController < ApplicationController
 	skip_before_filter :authenticate_user!
 	def index
+		if user_signed_in?
+			redirect_to dashboards_path
+		end
 		# @jobs = Job.published_jobs		
     if params[:search]
       @jobs = Job.search(params[:search]).order("created_at DESC")
