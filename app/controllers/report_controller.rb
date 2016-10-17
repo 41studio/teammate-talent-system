39316c7a0785ba3c02ca941aab5ba20c410a1115
Ilham <ilhamfi_2701@yahoo.com>
2016-10-17 17:01:04 +0700

    add ajax on radio button for filter

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,6 +1,20 @@
 class ReportController < ApplicationController
   def index
-  	@applied_applicant_by_day = Applicant.group('date(created_at)').count
+  	if params[:group_by] == "week"
+  		group = Applicant.group('week(created_at)')
+  	elsif params[:group_by] == "month"
+  		group = Applicant.group('month(created_at)')
+  	elsif params[:group_by] == "year"
+  		group = Applicant.group('year(created_at)')
+  	else
+  		group = Applicant.group('date(created_at)')
+  	end
+  	@applied_applicant_by_day = group.count
   	@jobs = current_user.company.jobs.where(job_status: "published")
+  	if params[:group_by].present?
+  		respond_to do |format|
+  			format.js { render 'report/index' }
+  		end
+  	end
   end
 end
