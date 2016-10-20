dc72a62056d4981ba7aebabc8f64276b5969976f
Ilham <ilhamfi_2701@yahoo.com>
2016-10-20 16:59:29 +0700

    add ajax filter on applicants page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -14,10 +14,40 @@ class DashboardsController < ApplicationController
 
   def applicant
     if current_user.company.present?
-     @jobs = current_user.company.jobs
-     @search = Applicant.search(params[:q])
-     @applicants = @search.result.where(job_id: @jobs).page(params[:page]).per(10)
-     @applicant_count = Applicant.total_applicant(current_user.company_id, @jobs)
+      @jobs = current_user.company.jobs
+      if params[:filter_by]
+        case params[:filter_by]
+        when "week"
+          @time = 1.week.ago
+        when "month"
+          @time = 1.month.ago
+        when "year"
+          @time = 1.year.ago
+        end
+      else
+        @time = Applicant.order("created_at ASC LIMIT 1").map(&:created_at)
+      end
+      
+      if params[:filter_by_stage]
+        filter_by_stage = params[:filter_by_stage].values
+      else
+        filter_by_stage = ["applied","phone_screen","interview","offer","hired"]
+      end
+    
+      if params[:filter_by_gender]
+        filter_by_gender = params[:filter_by_gender].values
+      else
+        filter_by_gender = ["Male","Female"]
+      end
+
+      @search = Applicant.search(params[:q])
+      @applicants = @search.result.where("job_id IN (?) and created_at >= ? and gender IN (?) and status IN (?)", @jobs.ids, @time, filter_by_gender, filter_by_stage).page(params[:page]).per(10)
+      @applicant_filter_result_count = @search.result.where("job_id IN (?) and created_at >= ? and gender IN (?) and status IN (?)", @jobs.ids, @time, filter_by_gender, filter_by_stage).count
+      @applicant_total = Applicant.total_applicant(current_user.company_id, @jobs).count
+      respond_to do |format|
+        format.html
+        format.js { render 'applicants/filter_applicant' }
+      end
     else
       flash[:notice] = "No Applicant here"
     end
