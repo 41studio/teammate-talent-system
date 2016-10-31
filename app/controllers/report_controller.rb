182e3c88aedfc97461eaa168dbc4642318ba6c51
Ilham <ilhamfi_2701@yahoo.com>
2016-10-31 13:37:53 +0700

    fix little bug

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,5 +1,5 @@
 class ReportController < ApplicationController
-  before_action :chart_data
+  before_action :chart_data, only: [:index, :download_report]
   def index
     @data = chart_data
     respond_to do |format|
@@ -23,53 +23,43 @@ class ReportController < ApplicationController
 
   private
     def chart_data
-      @jobs = current_user.company.jobs.published_jobs
+      @jobs = current_user.company.jobs.published_or_closed_jobs
       if params[:filter_by]
         case params[:filter_by]
         when "week"
-          @time = 'week(applicants.created_at)'
+          time = 'week(applicants.created_at)'
           @x_title = "Week"
         when "month"
-          @time = 'month(applicants.created_at)'
+          time = 'month(applicants.created_at)'
           @x_title = "Month"
         when "year"
-          @time = 'year(applicants.created_at)'
+          time = 'year(applicants.created_at)'
           @x_title = "Year"
         end
       else
-        @time = 'date(applicants.created_at)'
+        time = 'date(applicants.created_at)'
         @x_title = "Date"
       end
-      
+            
       if params[:filter_by_stage]
-        @filter_by_stage = params[:filter_by_stage].values
+        filter_by_stage = params[:filter_by_stage].values
+      elsif params[:filter_by_consideration].present? && params[:filter_by_consideration].values == Applicant::DISQUALIFIED
+        filter_by_stage = Applicant::DISQUALIFIED
       else
-        @filter_by_stage = ["applied","phone_screen","interview","offer","hired"]
+        filter_by_stage = Applicant::STATUSES.map{|key, val| key.to_s}
       end
 
-      if params[:filter_by_consideration] && params[:filter_by_consideration].values == "disqualified"
-        @filter_by_stage = params[:filter_by_consideration].values
-      else
-        if params[:filter_by_stage]
-          @filter_by_stage = params[:filter_by_stage].values
-        elsif params[:filter_by_consideration].present? && params[:filter_by_consideration].values == ["disqualified"]
-          @filter_by_stage = ["disqualified"]
-        else
-          @filter_by_stage = ["applied","phone_screen","interview","offer","hired"]
-        end
-      end
-      
       if params[:filter_by_job]
-        @filter_by_job = params[:filter_by_job].values
+        filter_by_job = params[:filter_by_job].values
       else
-        @filter_by_job = @jobs.job_title
+        filter_by_job = @jobs.ids
       end
-      
+
       if params[:filter_by_gender]
-        @filter_by_gender = params[:filter_by_gender].values
+        filter_by_gender = params[:filter_by_gender].values
       else
-        @filter_by_gender = ["Male","Female"]
+        filter_by_gender = ["Male","Female"]
       end
-      @data = Applicant.join_job.filter_report_applicant(current_user.company_id, @filter_by_job, @filter_by_stage, @filter_by_gender).group(@time).count
+      @data = Applicant.join_job.filter_report_applicant(current_user.company_id, filter_by_job, filter_by_stage, filter_by_gender).group(time).count
     end
 end
