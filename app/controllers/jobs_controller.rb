36cdb16a95b416db2a0dca2171759749d2debd98
Ilham <ilhamfi_2701@yahoo.com>
2017-01-03 08:00:59 +0700

    make the code tidier on jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -36,8 +36,6 @@ class JobsController < ApplicationController
   before_action :set_company, only: [:new]
   before_action :set_collection, only: [:new, :edit, :create, :update]
 
-  # GET /jobs
-  # GET /jobs.json
   def index
     @jobs = Job.all
     if params[:search]
@@ -47,57 +45,38 @@ class JobsController < ApplicationController
     end
   end
 
-  # GET /jobs/1
-  # GET /jobs/1.json
-  def show
-  end
+  def show; end
 
-  # GET /jobs/new
   def new
     @job = set_company.jobs.new
     @url = company_jobs_path(params[:company_id])
   end
 
-  # GET /jobs/1/edit
   def edit
     @job = set_company.jobs.find(set_job)
     @url = company_job_path(@job.company_id, @job)
   end
 
-  # POST /jobs
-  # POST /jobs.json
   def create
     @url = company_jobs_path(params[:company_id])
     @job = set_company.jobs.new(job_params)
     @job.status = "draft"
-    respond_to do |format|
-      if @job.save
-        format.html { redirect_to company_job_path(params[:company_id], @job), notice: 'Job was successfully created.' }
-        format.json { render :show, status: :created, location: @job }
-      else
-        format.html { render :new }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
-      end
+    if @job.save
+      redirect_to company_job_path(params[:company_id], @job), notice: 'Job was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /jobs/1
-  # PATCH/PUT /jobs/1.json
   def update
     @url = company_job_path(@job.company_id, @job)
-    respond_to do |format|
-      if @job.update(job_params)
-        format.html { redirect_to company_job_path(@job.company_id, @job), notice: 'Job was successfully updated.' }
-        format.json { render :show, status: :ok, location: @job }
-      else
-        format.html { render :edit }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
-      end
+    if @job.update(job_params)
+      redirect_to company_job_path(@job.company_id, @job), notice: 'Job was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /jobs/1
-  # DELETE /jobs/1.json
   def destroy
     @job.destroy
     respond_to do |format|
@@ -111,14 +90,10 @@ class JobsController < ApplicationController
     @job.status = params[:status]
     if @job.status == "published" or @job.status == "closed"
       if @job.save!
-        respond_to do |format|
-          format.html { redirect_to company_job_path(@job.company_id, @job), notice: 'Job was successfully '+@job.status+'.' }
-        end
+        redirect_to company_job_path(@job.company_id, @job), notice: 'Job was successfully '+@job.status+'.'
       end
     else
-      respond_to do |format|
-        format.html { redirect_to company_job_path(@job.company_id, @job), notice: 'Invalid command!' }
-      end
+      redirect_to company_job_path(@job.company_id, @job), notice: 'Invalid command!'
     end
   end
 
@@ -138,14 +113,12 @@ class JobsController < ApplicationController
       @employment_type_collection = EmploymentTypeList.all.collect {|i| [i.employment_type, i.id]}
       @industry_collection = IndustryList.all.collect {|i| [i.industry, i.id]}
       @education_collection = EducationList.all.collect {|i| [i.education, i.id]}
-      # @country_collection = CountryStateSelect.countries.collect {|k,v| [v, k.to_s]}
     end
 
     def user_allowed
       if current_user.company_id != set_company.id
         redirect_to dashboards_path
       end
-    end
     
     def job_allowed
       company_id = params[:company_id].to_i
@@ -153,7 +126,7 @@ class JobsController < ApplicationController
          redirect_to root_path, notice: 'No Job available' 
       end
     end
-
+  end
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
       params.require(:job).permit(:job_title, :departement, :job_code, :country, :state, :city, :zip_code, :min_salary, :max_salary, :curency, :job_description, :job_requirement, :benefits, :experience_list_id, :function_list_id, :employment_type_list_id, :industry_list_id, :education_list_id, :job_search_keyword)
