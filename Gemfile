204fa4a10761f0f1d2ae59457a54db2fbf956947
Ilham <ilhamfi_2701@yahoo.com>
2017-01-26 05:39:08 +0700

    add faker gem, remove slug column on applicant table, add method on job/show.html.slim

--- Gemfile
+++ Gemfile
@@ -137,4 +137,5 @@ group :development do
   gem 'letter_opener'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'faker'
 end
--- db/migrate/20170125062233_remove_applicants_slug.rb
+++ db/migrate/20170125062233_remove_applicants_slug.rb
@@ -1,4 +1,6 @@
 class RemoveApplicantsSlug < ActiveRecord::Migration
   def change
+    remove_column :applicants, :slug, :string
+    remove_index :applicants, :slug
   end
 end
